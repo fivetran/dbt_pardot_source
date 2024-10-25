a53e3672e9320ab4ec5a75bcdad708046ee04ba8
Tyler Wood <tylercwood@gmail.com>
2024-10-25 10:52:15 -0400

    union opportunity across schemas

--- macros/get_opportunity_columns.sql
+++ macros/get_opportunity_columns.sql
@@ -1,6 +1,7 @@
 {% macro get_opportunity_columns() %}
 
 {% set columns = [
+    {"name": "_dbt_source_relation", "datatype": dbt.type_string()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "campaign_id", "datatype": dbt.type_int()},
     {"name": "closed_at", "datatype": dbt.type_timestamp()},
--- models/stg_pardot__opportunity.sql
+++ models/stg_pardot__opportunity.sql
@@ -22,18 +22,25 @@ fields as (
 final as (
     
     select 
-        id as opportunity_id,
-        campaign_id,
-        created_at as created_timestamp,
-        updated_at as updated_timestamp,
-        name as opportunity_name,
+        {{generate_surrograte_key('_dbt_source_relation','id')}} as opportunity_surrogate_key,
+        
+        _dbt_source_relation,
+        {{parse_business_unit_from_schema('_dbt_source_relation')}} as pardot_business_unit_abbreviation,
+        
         probability,
         status as opportunity_status,
         stage,
         type as opportunity_type,
         value as amount,
+        
+        created_at as created_timestamp,
+        updated_at as updated_timestamp,
         _fivetran_synced,
         closed_at as closed_timestamp
+        
+        id as opportunity_id,
+        campaign_id,
+        name as opportunity_name
     from fields
 
 )
