a2b2eb44ce34b8fa73546320e7d874363537920e
Tyler Wood <tylercwood@gmail.com>
2024-10-25 13:04:24 -0400

    fix macro get campaign columns

--- macros/get_campaign_columns.sql
+++ macros/get_campaign_columns.sql
@@ -1,7 +1,7 @@
 {% macro get_campaign_columns() %}
 
 {% set columns = [
-    {"name": "_dbt_source_relation", "datatype": dbt.type_string()}
+    {"name": "_dbt_source_relation", "datatype": dbt.type_string()},
     {"name": "_fivetran_deleted", "datatype": "boolean"},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "cost", "datatype": dbt.type_int()},
