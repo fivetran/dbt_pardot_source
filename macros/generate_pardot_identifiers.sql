7e819c1188a3ac3f63cc3a30bd697c51c3f693d4
Tyler Wood <tylercwood@gmail.com>
2024-10-25 13:53:30 -0400

    macro for post-schema-union identifier columns

---
+++ macros/generate_pardot_identifiers.sql
@@ -0,0 +1,23 @@
+--creates creates a primary key as [model]_id from a surrogate made of the source relation + pre-union primary key
+--returns the _dbt_source_relation and pardot business unit abbreviation extracted from source schema
+--renames the original primary key as [model]_schema_specific_id 
+{% macro generate_pardot_identifiers(pre_union_primary_key, model_name=none) %}
+
+    {% set model = model_name if model_name else this.name %}
+
+    {{ dbt_utils.generate_surrogate_key(['_dbt_source_relation', pre_union_primary_key]) }} as {{ model }}_id,
+    _dbt_source_relation,
+    
+    {{ pre_union_primary_key }} as {{ model }}_schema_specific_id,
+
+    regexp_replace(
+        regexp_replace(
+            regexp_substr(_dbt_source_relation, 'PARDOT_(_?\\w+)', 1, 1, 'i', 1),
+            '^_',
+            ''
+        ),
+        '_',
+        ' '
+    ) as pardot_business_unit_abbreviation
+
+{% endmacro %}
