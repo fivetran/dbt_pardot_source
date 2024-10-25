26bff63e9465985cdd896bee74076225137fbb66
Tyler Wood <tylercwood@gmail.com>
2024-10-25 13:07:06 -0400

    add surrogate key to list email

--- models/stg_pardot__list_email.sql
+++ models/stg_pardot__list_email.sql
@@ -22,9 +22,10 @@ fields as (
 final as (
     
     select 
+        {{generate_surrograte_key('_dbt_source_relation','id')}} as list_email_surrogate_key,
+        
         _dbt_source_relation,
         parse_business_unit_from_schema(_dbt_source_relation) as pardot_business_unit_abbreviation,
-        id as list_email_id,
         
         /* basics */
         sent_at as list_email_sent_at,
@@ -183,7 +184,9 @@ final as (
 
         created_at as created_timestamp,
         updated_at as updated_timestamp,
-        _fivetran_synced
+        _fivetran_synced,
+        
+        id as list_email_id
     
     from fields
 
